dbb13c1dd7a2d993d32cc64855f9067020a54369
tacopo <charlicant@gmail.com>
2023-05-16 12:59:25 +0200

    Creada página inicio, componentes y primeros estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './componentes/vistas/home/home.component';
+import { GaleriaComponent } from './componentes/vistas/galeria/galeria.component';
+import { DivisasComponent } from './componentes/vistas/divisas/divisas.component';
+import { MaterialModule } from './modules/material/material.module';
+import { NavbarComponent } from './componentes/componentes/navbar/navbar.component';
+import { FooterComponent } from './componentes/componentes/footer/footer.component';
+import { CardComponent } from './componentes/componentes/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GaleriaComponent,
+    DivisasComponent,
+    NavbarComponent,
+    FooterComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/vistas/divisas/divisas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-divisas',
+  templateUrl: './divisas.component.html',
+  styleUrls: ['./divisas.component.scss']
+})
+export class DivisasComponent {
+
+}
